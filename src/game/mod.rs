12e3718e40991a57c734c6cb55886d1f2a454b6a
RubberDuckShobe <42943070+RubberDuckShobe@users.noreply.github.com>
2023-12-26 03:15:05 +0100

    Implement Steam friend sync

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -3,7 +3,7 @@ mod helpers;
 mod user;
 
 use crate::game::gameplay::{fetch_song_id, get_rides, send_ride};
-use crate::game::user::login_steam;
+use crate::game::user::{login_steam, steam_sync};
 use crate::AppState;
 use axum::routing::post;
 use axum::Router;
@@ -12,6 +12,7 @@ use axum::Router;
 pub fn routes_steam() -> Router<AppState> {
     Router::new()
         .route("/game_AttemptLoginSteamVerified.php", post(login_steam))
+        .route("/game_SteamSyncSteamVerified.php", post(steam_sync))
         .route("/game_fetchsongid_unicode.php", post(fetch_song_id))
         .route("/game_SendRideSteamVerified.php", post(send_ride))
         .route("/game_GetRidesSteamVerified.php", post(get_rides))
--- src/game/user.rs
+++ src/game/user.rs
@@ -1,9 +1,12 @@
 use crate::models::{NewPlayer, Player, SteamIdWrapper};
+use crate::schema::players::dsl::*;
 use crate::util::errors::{IntoRouteError, RouteError};
 use crate::AppState;
 use crate::{game::helpers::ticket_auth, schema::players};
 use axum::{extract::State, Form};
 use axum_serde::Xml;
+use diesel::query_dsl::methods::FilterDsl;
+use diesel::ExpressionMethods;
 use diesel_async::RunQueryDsl;
 use serde::{Deserialize, Serialize};
 use tracing::info;
@@ -24,7 +27,7 @@ pub struct LoginSteamResponse {
     #[serde(rename = "locationid")]
     location_id: i32,
     #[serde(rename = "steamid")]
-    steam_id: u32,
+    steam_id: i32,
 }
 
 /// Attempts to authenticate a user through Steam.
@@ -69,6 +72,62 @@ pub async fn login_steam(
         user_id: player.id,
         username: player.username,
         location_id: player.location_id,
-        steam_id: steam_player.get_account_id(),
+        steam_id: player.steam_account_num,
+    }))
+}
+
+#[derive(Deserialize)]
+pub struct SteamSyncRequest {
+    ticket: String,
+    snums: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename = "RESULTS")]
+pub struct SteamSyncResponse {
+    #[serde(rename = "@status")]
+    status: String,
+}
+
+pub async fn steam_sync(
+    State(state): State<AppState>,
+    Form(payload): Form<SteamSyncRequest>,
+) -> Result<Xml<SteamSyncResponse>, RouteError> {
+    //Split the string of steam account numbers into a vector
+    let friend_nums: Vec<i32> = payload
+        .snums
+        .split('x')
+        .map(str::parse)
+        .collect::<Result<Vec<i32>, _>>()
+        .http_status_error(axum::http::StatusCode::BAD_REQUEST)?;
+
+    let steam_player = ticket_auth(&payload.ticket, &state.steam_api)
+        .await
+        .http_internal_error("Failed to authenticate with Steam")?;
+
+    let mut conn = state.db.get().await?;
+
+    //Get all friends
+    let friends = players
+        .filter(steam_account_num.eq_any(&friend_nums))
+        .load::<Player>(&mut conn)
+        .await?;
+
+    //Add new rivalry for each friend
+    for friend in &friends {
+        diesel::insert_into(crate::schema::rivalries::table)
+            .values((
+                crate::schema::rivalries::player_id
+                    .eq(i32::try_from(steam_player.get_account_id())?),
+                crate::schema::rivalries::rival_id.eq(friend.steam_account_num),
+            ))
+            .on_conflict_do_nothing()
+            .execute(&mut conn)
+            .await?;
+    }
+
+    Ok(Xml(SteamSyncResponse {
+        //This technically doesn't return the number of friends added
+        status: format!("added {} of {} friends", friends.len(), friend_nums.len()),
     }))
 }
