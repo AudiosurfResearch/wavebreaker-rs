63c18d78ef4f653c917cf0f203c32feb798295e2
RubberDuckShobe <42943070+RubberDuckShobe@users.noreply.github.com>
2024-01-21 12:58:38 +0100

    Add fallback response for Audiosurf Radio

--- src/game/radio.rs
+++ src/game/radio.rs
@@ -7,10 +7,18 @@ use crate::util::{errors::RouteError, radio::get_radio_songs};
 /// That version is included with the Wavebreaker mod.
 #[instrument]
 pub async fn get_radio_list() -> Result<String, RouteError> {
+    let radio_songs = match get_radio_songs() {
+        Ok(songs) => songs,
+        Err(e) => {
+            tracing::error!("Failed to get radio songs: {}", e);
+            return Ok("no radio songs-:*x-This server has-:*x-none-:*x-https://github.com/AudiosurfResearch-:*x-".to_owned());
+        }
+    };
+
     // join all songs into a single string with -:*x- as separator
     // ignore the id, we don't need it
     let mut joined_string = String::new();
-    for song in get_radio_songs()? {
+    for song in radio_songs {
         joined_string.push_str(&format!(
             "{}-:*x-{}-:*x-{}-:*x-{}-:*x-",
             song.artist, song.title, song.cgr_url, song.buy_url
