e92e3f548a47fbeed4abfed4240949ff38dc2a12
RubberDuckShobe <42943070+RubberDuckShobe@users.noreply.github.com>
2024-02-01 22:00:49 +0100

    document and rename modifier stuff

--- src/util/modifiers.rs
+++ src/util/modifiers.rs
@@ -1,11 +1,16 @@
 use regex::Regex;
 
-pub fn tags_from_title(title: &str) -> Vec<&str> {
-    let full_tag_string_regex = Regex::new(r"(?:\[as-[a-zA-Z0-9]+\]\s*)+$").unwrap();
-    let tag_separation_regex = Regex::new(r"\[as-([a-zA-Z0-9]+)\]").unwrap();
+/// For song titles with modifiers, this function returns a vector of the modifiers.
+/// 
+/// **Example:** "death from above [as-steep]" -> ["steep"]
+/// 
+/// Returns an empty vector if no modifiers are found.
+pub fn modifiers_from_title(title: &str) -> Vec<&str> {
+    let full_tag_string_regex = Regex::new(r"(?:\[as-[a-zA-Z0-9]+\]\s*)+$").expect("Regex should always be valid!");
+    let tag_separation_regex = Regex::new(r"\[as-([a-zA-Z0-9]+)\]").expect("Regex should always be valid!");
 
     full_tag_string_regex.find(title).map_or_else(
-        || vec![],
+        std::vec::Vec::new,
         |tag_string| {
             tag_separation_regex
                 .captures_iter(tag_string.as_str())
@@ -15,8 +20,11 @@ pub fn tags_from_title(title: &str) -> Vec<&str> {
     )
 }
 
-pub fn remove_tags_from_title(title: &str) -> String {
-    let full_tag_string_regex = Regex::new(r"(?:\[as-[a-zA-Z0-9]+\]\s*)+$").unwrap();
+/// If the song title has modifiers, this function returns a new ``String`` without them.
+/// 
+/// **Example:** "death from above [as-steep]" -> "death from above"
+pub fn remove_modifiers_from_title(title: &str) -> String {
+    let full_tag_string_regex = Regex::new(r"(?:\[as-[a-zA-Z0-9]+\]\s*)+$").expect("Regex should always be valid!");
 
     full_tag_string_regex.find(title).map_or_else(
         || title.to_string(),
@@ -29,30 +37,30 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_tags_from_title_with_tags() {
+    fn mods_from_title() {
         let title = "My Title [as-tag1] [as-tag2] [as-steep]";
         let expected_tags = vec!["tag1", "tag2", "steep"];
-        assert_eq!(tags_from_title(title), expected_tags);
+        assert_eq!(modifiers_from_title(title), expected_tags);
     }
 
     #[test]
-    fn test_tags_from_title_without_tags() {
+    fn mods_from_title_empty() {
         let title = "My Title";
         let expected_tags: Vec<&str> = vec![];
-        assert_eq!(tags_from_title(title), expected_tags);
+        assert_eq!(modifiers_from_title(title), expected_tags);
     }
 
     #[test]
-    fn test_remove_tags_from_title_with_tags() {
+    fn remove_mods_from_title() {
         let title = "My Title [as-tag1] [as-tag2] [as-tag3]";
         let expected_result = "My Title";
-        assert_eq!(remove_tags_from_title(title), expected_result);
+        assert_eq!(remove_modifiers_from_title(title), expected_result);
     }
 
     #[test]
-    fn test_remove_tags_from_title_without_tags() {
+    fn remove_mods_from_title_empty() {
         let title = "My Title";
         let expected_result = "My Title";
-        assert_eq!(remove_tags_from_title(title), expected_result);
+        assert_eq!(remove_modifiers_from_title(title), expected_result);
     }
 }
