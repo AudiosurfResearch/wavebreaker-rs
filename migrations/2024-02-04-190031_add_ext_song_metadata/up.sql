d7360a6ac94eeee5326eded8a4feea3a0e7a3257
RubberDuckShobe <42943070+RubberDuckShobe@users.noreply.github.com>
2024-02-06 21:12:04 +0100

    tiny schema changes, re-patch schema

--- migrations/2024-02-04-190031_add_ext_song_metadata/up.sql
+++ migrations/2024-02-04-190031_add_ext_song_metadata/up.sql
@@ -7,5 +7,5 @@ ADD COLUMN musicbrainz_title TEXT,
 ADD COLUMN musicbrainz_artist TEXT,
 ADD COLUMN musicbrainz_length INT,
 ADD COLUMN mistag_lock BOOLEAN NOT NULL DEFAULT FALSE,
-ADD COLUMN aliases_artist TEXT[] NOT NULL DEFAULT ARRAY[]::TEXT[],
-ADD COLUMN aliases_title TEXT[] NOT NULL DEFAULT ARRAY[]::TEXT[];
+ADD COLUMN aliases_artist TEXT[],
+ADD COLUMN aliases_title TEXT[];
