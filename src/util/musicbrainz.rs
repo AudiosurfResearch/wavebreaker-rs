123b1da07ee9e647a4402fc9f1e1477f5e3bd199
RubberDuckShobe <42943070+RubberDuckShobe@users.noreply.github.com>
2024-04-07 00:51:39 +0200

    Add `lookup_mbid` to `musicbrainz` module, closes #9

--- src/util/musicbrainz.rs
+++ src/util/musicbrainz.rs
@@ -1,7 +1,6 @@
 use diesel::{prelude::Insertable, query_builder::AsChangeset};
 use musicbrainz_rs::{
-    entity::{recording::Recording, CoverartResponse},
-    FetchCoverart, Search,
+    entity::{recording::Recording, CoverartResponse}, Fetch, FetchCoverart, Search
 };
 use tracing::info;
 
@@ -38,6 +37,59 @@ pub async fn lookup_metadata(song: &Song, duration: i32) -> anyhow::Result<Music
     }
 
     let recording = query_result[0].clone();
+    let release = recording.releases.clone();
+    let release = if let Some(releases) = release {
+        releases[0].clone()
+    } else {
+        return Err(anyhow::anyhow!("No release found for recording"));
+    };
+
+    let cover_url = release.get_coverart().front().res_500().execute().await?;
+    let cover_url = match cover_url {
+        CoverartResponse::Json(cover) => cover.images[0].image.clone(),
+        CoverartResponse::Url(url) => url,
+    };
+
+    let cover_url_small = release.get_coverart().front().res_250().execute().await?;
+    let cover_url_small = match cover_url_small {
+        CoverartResponse::Json(cover) => cover.images[0].image.clone(),
+        CoverartResponse::Url(url) => url,
+    };
+
+    let mbid = recording.id;
+    let musicbrainz_title = recording.title;
+    let musicbrainz_artist = recording.artist_credit;
+    let musicbrainz_artist = if let Some(artist_credit) = musicbrainz_artist {
+        // Join all artists by their join phrase
+        let mut artist_string = String::new();
+        for artist in artist_credit {
+            artist_string.push_str(&artist.name);
+            if let Some(join_phrase) = artist.joinphrase {
+                artist_string.push_str(&join_phrase);
+            }
+        }
+        artist_string
+    } else {
+        return Err(anyhow::anyhow!("No artist found for recording"));
+    };
+
+    //let's be real, we're not gonna see a song be so long it eclipses i32::MAX
+    #[allow(clippy::cast_possible_wrap)]
+    let musicbrainz_length = recording.length.map(|length| length as i32);
+
+    Ok(MusicBrainzInfo {
+        cover_url,
+        cover_url_small,
+        mbid,
+        musicbrainz_title,
+        musicbrainz_artist,
+        musicbrainz_length: musicbrainz_length.unwrap_or_default(),
+    })
+}
+
+pub async fn lookup_mbid(mbid: &str) -> anyhow::Result<MusicBrainzInfo> {
+    let recording = Recording::fetch().id(mbid).execute().await?;
+
     let release = recording.releases.clone();
     let release = if let Some(releases) = release {
         releases[0].clone()
