e7ac76f7a51312edfcfbbbaffca997c1837eee47
RubberDuckShobe <42943070+RubberDuckShobe@users.noreply.github.com>
2024-07-02 15:38:32 +0200

    Add song and score deletion CLI commands

--- src/manager/mod.rs
+++ src/manager/mod.rs
@@ -1,6 +1,7 @@
 use clap::{ArgAction, Parser, Subcommand};
 use diesel::prelude::*;
 use diesel_async::RunQueryDsl;
+use tracing::instrument;
 
 use crate::AppState;
 
@@ -19,8 +20,16 @@ pub enum Command {
         #[clap(action=ArgAction::Set)]
         new_alias: bool,
     },
+    DeleteSong {
+        id_to_delete: i32,
+    },
+    DeleteScore {
+        id_to_delete: i32,
+    },
 }
 
+//skip state because it has members that don't implement Debug
+#[instrument(name = "cli_command", skip(state))]
 pub async fn parse_command(command: &Command, state: AppState) -> anyhow::Result<()> {
     match command {
         Command::MergeSongs {
@@ -37,6 +46,30 @@ pub async fn parse_command(command: &Command, state: AppState) -> anyhow::Result
             to_merge
                 .merge_into(*target, *new_alias, &mut conn, &mut redis_conn)
                 .await
+        }
+        Command::DeleteSong { id_to_delete } => {
+            use crate::schema::songs::dsl::*;
+
+            let mut conn = state.db.get().await?;
+            let mut redis_conn = state.redis.get().await?;
+
+            let song = songs
+                .find(*id_to_delete)
+                .first::<crate::models::songs::Song>(&mut conn)
+                .await?;
+            song.delete(&mut conn, &mut redis_conn).await
+        }
+        Command::DeleteScore { id_to_delete } => {
+            use crate::schema::scores::dsl::*;
+
+            let mut conn = state.db.get().await?;
+            let mut redis_conn = state.redis.get().await?;
+
+            let score_to_delete = scores
+                .find(*id_to_delete)
+                .first::<crate::models::scores::Score>(&mut conn)
+                .await?;
+            score_to_delete.delete(&mut conn, &mut redis_conn).await
         }
     }
 }
