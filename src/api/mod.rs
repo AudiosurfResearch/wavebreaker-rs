61488bfd50ba985f7454bfa9a199d732c74dbd7b
RubberDuckShobe <42943070+RubberDuckShobe@users.noreply.github.com>
2024-11-02 15:29:47 +0100

    API doc changes

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,6 +1,12 @@
 use axum::{Json, Router};
 use serde::Serialize;
-use utoipa::{openapi::OpenApi, OpenApi as OpenApiTrait, ToSchema};
+use utoipa::{
+    openapi::{
+        security::{HttpAuthScheme, HttpBuilder, SecurityScheme},
+        OpenApi,
+    },
+    Modify, OpenApi as OpenApiTrait, ToSchema,
+};
 use utoipa_axum::{router::OpenApiRouter, routes};
 
 use crate::{
@@ -14,11 +20,31 @@ mod rivals;
 mod songs;
 
 #[derive(OpenApiTrait)]
-#[openapi(servers((url = "/api")), security(
+#[openapi(
+    modifiers(&SecurityAddon),
+    servers((url = "/api")), security(
     (),
     ("token_jwt" = [])
 ))]
 pub struct ApiDoc;
+
+struct SecurityAddon;
+
+impl Modify for SecurityAddon {
+    fn modify(&self, openapi: &mut utoipa::openapi::OpenApi) {
+        if let Some(components) = openapi.components.as_mut() {
+            components.add_security_scheme(
+                "token_jwt",
+                SecurityScheme::Http(
+                    HttpBuilder::new()
+                        .scheme(HttpAuthScheme::Bearer)
+                        .bearer_format("JWT")
+                        .build(),
+                ),
+            )
+        }
+    }
+}
 
 pub fn routes() -> (Router<AppState>, OpenApi) {
     OpenApiRouter::with_openapi(ApiDoc::openapi())
@@ -37,12 +63,17 @@ struct HealthCheck {
     radio_status: String,
 }
 
-/// Get health of the API.
+/// Get health of the server
 #[utoipa::path(
     method(get),
     path = "/healthCheck",
     responses(
-        (status = OK, description = "Success", body = HealthCheck, content_type = "application/json")
+        (status = OK, description = "Success",
+        body = HealthCheck, content_type = "application/json",
+        examples = (json!(r#"{ "status": "ok", "radioStatus": "2 song(s)" }"#))),
+        (status = OK, description = "Server works, but Radio is broken",
+        body = HealthCheck, content_type = "application/json",
+        example = json!(r#"{ "status": "ok", "radioStatus": "error" }"#)),
     )
 )]
 async fn health_check() -> Result<Json<HealthCheck>, RouteError> {
