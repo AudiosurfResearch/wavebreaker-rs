588bd5a59473ea9baff846bdafad29bcaa2be995
RubberDuckShobe <42943070+RubberDuckShobe@users.noreply.github.com>
2024-12-21 02:14:24 +0100

    Add endpoint for removing rivals

--- src/api/rivals.rs
+++ src/api/rivals.rs
@@ -9,7 +9,10 @@ use utoipa::ToSchema;
 use utoipa_axum::{router::OpenApiRouter, routes};
 
 use crate::{
-    models::{players::Player, rivalries::{NewRivalry, Rivalry, RivalryView}},
+    models::{
+        players::Player,
+        rivalries::{NewRivalry, Rivalry, RivalryView},
+    },
     util::{
         errors::{RouteError, SimpleRouteErrorOutput},
         jwt::Claims,
@@ -18,7 +21,10 @@ use crate::{
 };
 
 pub fn routes() -> OpenApiRouter<AppState> {
-    OpenApiRouter::new().routes(routes!(get_own_rivals)).routes(routes!(add_rival))
+    OpenApiRouter::new()
+        .routes(routes!(get_own_rivals))
+        .routes(routes!(add_rival))
+        .routes(routes!(remove_rival))
 }
 
 #[derive(Serialize, ToSchema)]
@@ -54,7 +60,7 @@ async fn get_own_rivals(
 
 #[derive(Deserialize)]
 #[serde(rename_all = "camelCase")]
-struct AddRivalParams {
+struct ModifyRivalParams {
     rival_id: i32,
 }
 
@@ -66,7 +72,7 @@ struct AddRivalParams {
         ("rivalId" = i32, Query, description = "ID of player to add as rival")
     ),
     responses(
-        (status = OK, description = "Success"),
+        (status = OK, body = RivalryView, description = "Success", content_type = "application/json"),
         (status = NOT_FOUND, description = "Couldn't find player to rival", body = SimpleRouteErrorOutput, content_type = "application/json"),
         (status = BAD_REQUEST, description = "Invalid parameters", body = SimpleRouteErrorOutput, content_type = "application/json"),
         (status = CONFLICT, description = "Rivalry already exists", body = SimpleRouteErrorOutput, content_type = "application/json"),
@@ -78,11 +84,10 @@ struct AddRivalParams {
 ]
 async fn add_rival(
     State(state): State<AppState>,
-    query: Query<AddRivalParams>,
+    query: Query<ModifyRivalParams>,
     claims: Claims,
 ) -> Result<Json<RivalryView>, RouteError> {
-    use crate::schema::players::dsl::*;
-    use crate::schema::rivalries::dsl::*;
+    use crate::schema::{players::dsl::*, rivalries::dsl::*};
 
     let mut conn = state.db.get().await?;
 
@@ -103,13 +108,71 @@ async fn add_rival(
 
     if rivalry.is_some() {
         Err(RouteError::new_conflict().set_public_error_message("Rivalry already exists"))
-    }
-    else {
+    } else {
         let new_rivalry = NewRivalry {
             challenger_id: player.id,
             rival_id: rival.id,
-        }.create(&mut conn).await?;
+        }
+        .create(&mut conn)
+        .await?;
 
-        Ok(Json(RivalryView::from_rivalry(new_rivalry, &mut conn).await?))
+        Ok(Json(
+            RivalryView::from_rivalry(new_rivalry, &mut conn).await?,
+        ))
     }
+}
+
+/// Get own rivals
+#[utoipa::path(
+    method(delete),
+    path = "/remove",
+    params(
+        ("rivalId" = i32, Query, description = "ID of player to un-rival")
+    ),
+    responses(
+        (status = OK, description = "Success"),
+        (status = NOT_FOUND, description = "Couldn't find player to un-rival or they aren't a rival", body = SimpleRouteErrorOutput, content_type = "application/json"),
+        (status = BAD_REQUEST, description = "Invalid parameters", body = SimpleRouteErrorOutput, content_type = "application/json"),
+        (status = UNAUTHORIZED, description = "Unauthorized", body = SimpleRouteErrorOutput, content_type = "application/json")
+    ),
+    security(
+        ("token_jwt" = [])
+    ))
+]
+async fn remove_rival(
+    State(state): State<AppState>,
+    query: Query<ModifyRivalParams>,
+    claims: Claims,
+) -> Result<(), RouteError> {
+    use crate::schema::{players::dsl::*, rivalries::dsl::*};
+
+    let mut conn = state.db.get().await?;
+
+    let player: Player = players.find(claims.profile.id).first(&mut conn).await?;
+    let rival: Player = players
+        .find(query.rival_id)
+        .first(&mut conn)
+        .await
+        .optional()?
+        .ok_or_else(|| RouteError::new_not_found().set_public_error_message("Player not found"))?;
+
+    rivalries
+        .filter(rival_id.eq(rival.id))
+        .filter(challenger_id.eq(player.id))
+        .first::<Rivalry>(&mut conn)
+        .await
+        .optional()?
+        .ok_or_else(|| {
+            RouteError::new_not_found().set_public_error_message("Player is not a rival")
+        })?;
+
+    diesel::delete(
+        rivalries
+            .filter(rival_id.eq(rival.id))
+            .filter(challenger_id.eq(player.id)),
+    )
+    .execute(&mut conn)
+    .await?;
+
+    Ok(())
 }
